3d94fd9428add3936cef9142481aa96740b8fa3a
the-code-genin <adekunle3317@gmail.com>
2022-10-15 03:39:02 +0100

    Refactored flags

--- cli/flags.go
+++ cli/flags.go
@@ -4,7 +4,7 @@ import "github.com/urfave/cli/v2"
 
 var (
 	keySizeFlag = &cli.Uint64Flag{
-		Name:       "size",
+		Name:       "key.size",
 		Aliases:    []string{"s"},
 		Usage:      "The key size in bits. Must be a multiple of 32 bits.",
 		Value:      32,
@@ -12,8 +12,22 @@ var (
 		HasBeenSet: true,
 	}
 
-	formatFlag = &cli.StringFlag{
-		Name:       "format",
+	keyFileFlag = &cli.StringFlag{
+		Name:     "key.file",
+		Usage:    "The key file path.",
+		Required: true,
+	}
+
+	keyFormatFlag = &cli.StringFlag{
+		Name:       "key.format",
+		Usage:      "The key file format. Available options are \"binary\", \"hex\" and \"base64\".",
+		Value:      "hex",
+		Required:   true,
+		HasBeenSet: true,
+	}
+
+	outputFormatFlag = &cli.StringFlag{
+		Name:       "output.format",
 		Aliases:    []string{"f"},
 		Usage:      "The output format. Available options are \"binary\", \"hex\" and \"base64\".",
 		Value:      "hex",
@@ -21,8 +35,8 @@ var (
 		HasBeenSet: true,
 	}
 
-	outputFlag = &cli.StringFlag{
-		Name:    "output",
+	outputFileFlag = &cli.StringFlag{
+		Name:    "output.file",
 		Aliases: []string{"o"},
 		Usage:   "The output file path. Required for \"binary\" output format",
 	}
