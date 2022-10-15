35332e73a21237a06ddcc2402595d440e9f820d5
the-code-genin <adekunle3317@gmail.com>
2022-10-15 01:58:24 +0100

    Refactored encryption and decryption algorithm

--- core/cipher.go
+++ core/cipher.go
@@ -1,6 +1,9 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type GESCipher struct {
 	binary    Binary
@@ -44,6 +47,35 @@ func (c *GESCipher) encrypt(block []byte, key []byte) ([]byte, error) {
 	return output, nil
 }
 
+func (c *GESCipher) Encrypt(data []byte, key []byte) ([]byte, error) {
+	// Validate key size
+	if requiredKeySize := int(c.blockSize/2) / 8; len(key) != requiredKeySize {
+		return nil, fmt.Errorf("key size must be %v bits", requiredKeySize*8)
+	}
+
+	// Pad the input data
+	paddedData, err := c.binary.PadBytes(data, c.blockSize)
+	if err != nil {
+		return nil, err
+	}
+
+	// Run the ECB method
+	output := make([]byte, 0)
+	blockBytes := int(c.blockSize / 8)
+	noBlocks := len(paddedData) / blockBytes
+	for i := 0; i < noBlocks; i++ {
+		offset := i * blockBytes
+		cipherText, err := c.encrypt(data[offset:offset+blockBytes], key)
+		if err != nil {
+			return nil, err
+		}
+
+		output = append(output, cipherText...)
+	}
+
+	return output, nil
+}
+
 func (c *GESCipher) runDecryption(
 	leftBlock []byte,
 	rightBlock []byte,
@@ -77,6 +109,42 @@ func (c *GESCipher) decrypt(block []byte, key []byte) ([]byte, error) {
 	output := make([]byte, 0)
 	output = append(output, leftBlock...)
 	output = append(output, rightBlock...)
+
+	return output, nil
+}
+
+func (c *GESCipher) Decrypt(data []byte, key []byte) ([]byte, error) {
+	// Validate key size
+	if requiredKeySize := int(c.blockSize/2) / 8; len(key) != requiredKeySize {
+		return nil, fmt.Errorf("key size must be %v bits", requiredKeySize*8)
+	}
+
+	buffer := make([]byte, 0)
+	buffer = append(buffer, data...)
+
+	// Pad the input data with null bytes if needed
+	requiredBlockBytes := int(c.blockSize / 8)
+	inputDataBytes := len(data)
+	noBlocks := int(math.Ceil(float64(inputDataBytes) / float64(requiredBlockBytes)))
+	if inputDataBytes%requiredBlockBytes != 0 {
+		fillableBytes := make([]byte, (noBlocks*requiredBlockBytes)-inputDataBytes)
+		buffer = append(buffer, fillableBytes...)
+	}
+
+	// Run the ECB method
+	output := make([]byte, 0)
+	for i := 0; i < noBlocks; i++ {
+		offset := i * requiredBlockBytes
+		plainText, err := c.decrypt(buffer[offset:offset+requiredBlockBytes], key)
+		if err != nil {
+			return nil, err
+		}
+
+		output = append(output, plainText...)
+	}
+
+	return output, nil
+}
 
 func NewGESCipher(blockSize uint64) (*GESCipher, error) {
 	if blockSize%8 != 0 {
