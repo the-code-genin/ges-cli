b681f516163fcc269db6328e1ef33102ac4c40b5
the-code-genin <adekunle3317@gmail.com>
2022-10-15 04:21:22 +0100

    Updated cipher tests

--- core/cipher_test.go
+++ core/cipher_test.go
@@ -19,8 +19,36 @@ func FuzzGESCipher(f *testing.F) {
 	}
 
 	f.Add([]byte("Hello world"))
-	f.Add([]byte("foo bar"))
-	f.Add([]byte{6, 2, 3})
+
+	tinyBlock, err := RandomBytes(16)
+	if err != nil {
+		f.Error(err)
+	}
+	f.Add(tinyBlock)
+
+	smallBlock, err := RandomBytes(48)
+	if err != nil {
+		f.Error(err)
+	}
+	f.Add(smallBlock)
+
+	midBlock, err := RandomBytes(256)
+	if err != nil {
+		f.Error(err)
+	}
+	f.Add(midBlock)
+
+	largeBlock, err := RandomBytes(512)
+	if err != nil {
+		f.Error(err)
+	}
+	f.Add(largeBlock)
+
+	superLargeBlock, err := RandomBytes(2560)
+	if err != nil {
+		f.Error(err)
+	}
+	f.Add(superLargeBlock)
 
 	f.Fuzz(func(t *testing.T, tc []byte) {
 		cipherText, err := cipher.Encrypt(tc, key)
