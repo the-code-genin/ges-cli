9e173200cc6169f1549ded2da441e4508a8f0f05
mohammed.adekunle <adekunle3317@gmail.com>
2024-07-11 04:45:25 +0100

    Cleaned up core

--- core/cipher_test.go
+++ core/cipher_test.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -33,65 +32,4 @@ func TestCipher(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, data, plainBlock)
-}
-
-func FuzzGESCipher(f *testing.F) {
-	cipher, err := NewGESCipher()
-	if err != nil {
-		f.Error(err)
-	}
-
-	// Generate a random 64-bit key
-	key, err := internal.RandomBytes(cipher.blockSize / 16)
-	if err != nil {
-		f.Error(err)
-	}
-
-	f.Add([]byte("Hello world"))
-
-	tinyBlock, err := internal.RandomBytes(16)
-	if err != nil {
-		f.Error(err)
-	}
-	f.Add(tinyBlock)
-
-	smallBlock, err := internal.RandomBytes(48)
-	if err != nil {
-		f.Error(err)
-	}
-	f.Add(smallBlock)
-
-	midBlock, err := internal.RandomBytes(256)
-	if err != nil {
-		f.Error(err)
-	}
-	f.Add(midBlock)
-
-	largeBlock, err := internal.RandomBytes(512)
-	if err != nil {
-		f.Error(err)
-	}
-	f.Add(largeBlock)
-
-	superLargeBlock, err := internal.RandomBytes(2560)
-	if err != nil {
-		f.Error(err)
-	}
-	f.Add(superLargeBlock)
-
-	f.Fuzz(func(t *testing.T, tc []byte) {
-		cipherText, err := cipher.Encrypt(tc, key)
-		if err != nil {
-			t.Error(err)
-		}
-
-		plainText, err := cipher.Decrypt(cipherText, key)
-		if err != nil {
-			t.Error(err)
-		}
-
-		if !bytes.Equal(plainText, tc) {
-			t.Errorf("expected %v to match %v", plainText, tc)
-		}
-	})
 }
