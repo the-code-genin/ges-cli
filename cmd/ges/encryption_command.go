dcc5f5b0c98274270c62f26c90eabb442d9b8186
mohammed.adekunle <adekunle3317@gmail.com>
2024-07-11 05:38:23 +0100

    Bug fix

--- cmd/ges/encryption_command.go
+++ cmd/ges/encryption_command.go
@@ -75,7 +75,7 @@ func encryptionAction(ctx *cli.Context) error {
 	// Writing to specified file
 	if outputFilePath != "" {
 		var err error
-		outputStream, err = os.OpenFile(inputFilePath, os.O_WRONLY|os.O_CREATE, 0755)
+		outputStream, err = os.OpenFile(outputFilePath, os.O_WRONLY|os.O_CREATE, 0755)
 		if err != nil {
 			return err
 		}
